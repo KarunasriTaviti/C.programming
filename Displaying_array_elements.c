ce0e36505efe808f43c35dc47c01066cd332ebff
Karunasri Taviti <105840346+KarunasriTaviti@users.noreply.github.com>
2026-04-20 19:22:19 +0530

    Added program to display array elements
    This program takes input of 5 numbers into an array and displays them.

---
+++ Displaying_array_elements.c
@@ -0,0 +1,18 @@
+//taking input of elements into an array and displaying them
+#include <stdio.h>
+
+int main() {
+    int arr[5],i;
+
+    printf("Enter 5 numbers:\n");
+    for(i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Array elements are:\n");
+    for(i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
